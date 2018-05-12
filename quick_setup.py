4d790fdcd7ce29c5858483f228e16078c41f8921
Dylan Randall <dylan.d.randall@gmail.com>
2018-05-12 19:59:33 +1000

    Updated quick setup & dependencies.txt

--- quick_setup.py
+++ quick_setup.py
@@ -9,19 +9,19 @@ def check_project_structure(modules, packages, dirs):
     for dir in dirs:
         path = os.path.join(root, dir)
         if not os.path.exists(path) or not os.path.isdir(path):
-            sys.stderr.write('directory: {} is invalid!\n'.format(dir))
+            sys.stderr.write('directory: {} is missing / invalid!\n'.format(dir))
             sys.exit(2)
 
     for package in packages:
         path = os.path.join(root, package)
         if not os.path.exists(path) or not os.path.isdir(path) or not os.path.exists(os.path.join(path, '__init__.py')):
-            sys.stderr.write('package: {} is invalid!\n'.format(dir))
+            sys.stderr.write('package: {} is missing / invalid!\n'.format(dir))
             sys.exit(3)
 
     for module in modules:
         path = os.path.join(root, module)
         if not os.path.exists(path) or not os.path.isfile(path):
-            sys.stderr.write('module: {} is invalid!\n'.format(module))
+            sys.stderr.write('module: {} is missing / invalid!\n'.format(module))
             sys.exit(4)
 
     sys.stdout.write('project...OK\n')
@@ -37,11 +37,19 @@ def parse_dependencies():
     with open(path, 'r') as f:
         line = f.readline()
         while line:
-            if '#' in line:  # remove comments
-                line = line.split('#')[0]
+            hashtag_index = -1
+            for i in range(0, len(line)):
+                if line[i] == '#':
+                    hashtag_index = i
+                    break
 
-            line = line.strip().split(':')
-            dependencies.append((line[0], line[1]))
+            if hashtag_index >= 0:
+                line = line[:hashtag_index]
+
+            if len(line) > 0:
+                if line[0] != '#':
+                    line = line.strip()
+                    dependencies.append(line)
 
             line = f.readline()
 
@@ -49,17 +57,13 @@ def parse_dependencies():
 
 
 def exec_pip(dependency):
-    command = ''
-    if dependency[1] == '-1':
-        command = ['python', '-m', 'pip', 'install', dependency[0]]
-    else:
-        command = ['python', '-m', 'pip', 'install', dependency[0] + dependency[1]]
+    command = ['python', '-m', 'pip', 'install', dependency]
 
     try:
         subprocess.check_call(command, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        sys.stdout.write(dependency[0] + '...OK\n')
+        sys.stdout.write(dependency + '...OK\n')
     except subprocess.CalledProcessError as e:
-        sys.stderr.write(str(e) + '\n' + dependency[0] + '...BAD\n')
+        sys.stderr.write(str(e) + '\n' + dependency + '...BAD\n')
 
 
 title = 'Python 3 Project template'
@@ -76,19 +80,30 @@ modules = ['definitions.py', 'dependencies.txt']
 sys.stdout.write('checking project structure...\n')
 check_project_structure(modules, packages, dirs)
 
-sys.stdout.write('\nsetting up dependencies...\n')
-for dependency in parse_dependencies():
-    exec_pip(dependency)
+dependencies = parse_dependencies()
+if len(dependencies) > 0:
+    sys.stdout.write('\nsetting up dependencies... (this may take some time)\n')
+    for dependency in dependencies:
+        exec_pip(dependency)
 
-sys.stdout.write('\nquick setup finished\n')
-sys.stdout.write('\nDetails\n-------\n')
-sys.stdout.write('Title: ' + title + '\n')
-sys.stdout.write('Version: ' + version + '\n')
-sys.stdout.write('Description: ' + description + '\n')
-sys.stdout.write('Author: ' + author + '\n')
-sys.stdout.write('Author\'s Email: ' + author + '\n')
+details = 'Details\n-------\nTitle: ' + title + '\nVersion: ' + version + '\n' + 'Description: ' + description + '\n' \
+          + 'Author: ' + author + '\n' + 'Author\'s Email: ' + author + '\n'
 
 if download_url:
-    sys.stdout.write('Download URL: ' + download_url + '\n')
+    details += 'Download URL: ' + download_url + '\n'
 
-# TODO ADD ABILITY TO SAVE DETAILS TO FILE
+if len(dependencies) > 0:
+    details += '\nDependencies:\n'
+    for dependency in dependencies:
+        details += dependency + '\n'
+
+sys.stdout.write('\nquick setup finished\n\n' + details)
+save_details = input("\nsave details to INFO.txt ? [y/n]: ")
+save_details = save_details.lower()
+
+if len(save_details) > 0:
+    if save_details[0] == 'y':
+        with open('INFO.txt', 'w+') as f:
+            f.write(details)
+
+sys.stdout.write("\nfinished\n")
